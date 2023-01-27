129489693875ae35b3c93de2796ecce3bb03b087
elyass546 <ilyaseabsi@gmail.com>
2023-01-27 10:46:07 -0500

    mandatory is done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# create directory to use in nginx container later and also to setup the wordpress conf
 mkdir /var/www/
 mkdir /var/www/html
 
@@ -19,34 +22,23 @@ mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 mv /wp-config.php /var/www/html/wp-config.php
 
-
 sed -i -r "s/db1/$db_name/1"   wp-config.php
 sed -i -r "s/user/$db_user/1"  wp-config.php
 sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$wp_url/ --title=$wp_title --admin_user=$wp_admin --admin_password=$wp_admin_pwd --admin_email=$wp_admin_email --skip-email --allow-root
 
+wp user create $wp_user $wp_user_email --role=author --user_pass=$wp_user_pwd --allow-root
 
-
-
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-
-wp theme install astra --activate --allow-root
-
-
-wp plugin install redis-cache --activate --allow-root
+wp user create $wp_test $wp_test_email --role=author --user_pass=$wp_test --allow-root
 
 wp plugin update --all --allow-root
 
+wp theme install twentytwentytwo --activate --allow-root
 
- 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir /run/php
 
-
-
-wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.3 -F
